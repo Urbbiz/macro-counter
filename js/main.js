dc2feca157e568326978ec95b075017917e64f50
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-05-21 15:48:07 +0300

    hamburger animation added

--- js/main.js
+++ js/main.js
@@ -4,14 +4,6 @@ import { Diary } from './components/Diary.js';
 import { EditForm } from './components/EditForm.js';
 
 
-
-
-//Hamburger
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-
 const addNewButton = document.querySelector('.add-new');
 const lightbox = document.querySelector('.lightbox');
 
@@ -22,8 +14,6 @@ const newFat = document.getElementById('new-fat');
 const newkcal = document.getElementById('new-kcal');
 const buttonCancelAdd = document.getElementById('button-cancel-add');
 const buttonAdd = document.getElementById('button-add');
-
-
 
 
 // Init objects
@@ -60,19 +50,9 @@ buttonCancelAdd.addEventListener('click', e => {
 
 buttonAdd.addEventListener('click', e => {
     e.preventDefault();
-    // const meal = {
-    //     id: ++diary.lastCreatedMealId,
-    //     name: newName.value,
-    //     carb: newCarb.value,
-    //     protein: newProtein.value,
-    //     fat: newFat.value,
-    //     kcal: newkcal.value,
-    // }
     diary.addMeal(newName.value, newCarb.value, newProtein.value, newFat.value, newkcal.value, );
     diary.clearAddForm();
     lightbox.classList.remove('show');
-
-    // console.log('add suveike');
 })
 
 addEventListener('keyup', ({ key }) => { //spaudzian escape visada uzdarys forma
@@ -85,17 +65,21 @@ addEventListener('keyup', ({ key }) => { //spaudzian escape visada uzdarys forma
 
 //hamburger events 
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active')
+// });
+//Hamburger
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        navbarLinks.classList.toggle('active')
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+        navbarLinks.classList.toggle('active')
+    }
 });
-
-
-
-//Footer Year changer
-// const year = new Date().getFullYear();
-// const date = `<p>${year}: &copy;</p>
-// <a href="https://www.linkedin.com/in/andrius-urbonas-45b1a433/" target="blank">
-//     <img class="img" src="./img/footer/logo.png" alt="logo">`;
-// document.getElementById('date-js').innerHTML = date;
-
-//Footer visitors counter
