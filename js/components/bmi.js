4d5af39e6f44c1332041f07c15bcc7bc24e7139f
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-05-24 08:43:16 +0300

    commit

--- js/components/bmi.js
+++ js/components/bmi.js
@@ -9,13 +9,19 @@ document.querySelector('.calculate .gender .male').onclick = function() {
 }
 
 document.querySelector('.calculate .gender .female').onclick = function() {
-    gender = 'female';
-    this.classList.add('active');
-    document.querySelector('.calculate .gender .male').className = 'male';
+        gender = 'female';
+        this.classList.add('active');
+        document.querySelector('.calculate .gender .male').className = 'male';
 
-}
-
+    }
+    // keicia skaicius keiciant input reiksme.
 document.querySelector('.calculate .height input').onchange = function() {
     height = parseInt(this.value);
+    document.querySelector('.calculate .height .val span').innerText = height;
+}
+
+// spaudziant ant + dideja skaicius
+document.querySelector('.calculate .height .val i.add-bmi').onclick = function() {
+    height += 1;
     document.querySelector('.calculate .height .val span').innerText = height;
 }
